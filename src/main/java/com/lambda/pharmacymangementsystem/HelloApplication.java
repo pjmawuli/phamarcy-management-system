5b1105f4ae5b3666867f615fa596a1a962ea34f4
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-07-04 19:37:19 +0000

    update add-purchase-view.fxml

--- src/main/java/com/lambda/pharmacymangementsystem/HelloApplication.java
+++ src/main/java/com/lambda/pharmacymangementsystem/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/fxml/add-purchases-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/lambda/pharmacymangementsystem/controller/AddPurchasesController.java
@@ -0,0 +1,10 @@
+package com.lambda.pharmacymangementsystem.controller;
+
+import javafx.event.ActionEvent;
+
+public class AddPurchasesController {
+    public void updateTotalPrice(ActionEvent actionEvent) {
+    }
+
+    public void addPurchase(ActionEvent actionEvent) {}
+}
