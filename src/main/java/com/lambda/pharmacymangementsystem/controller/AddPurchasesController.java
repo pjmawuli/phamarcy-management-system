8e588553f3da337517b54e803bd4d46cea135b4e
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-07-13 14:43:13 +0000

    Populate the form with drugs.

--- src/main/java/com/lambda/pharmacymangementsystem/controller/AddPurchasesController.java
+++ src/main/java/com/lambda/pharmacymangementsystem/controller/AddPurchasesController.java
@@ -1,10 +1,16 @@
 package com.lambda.pharmacymangementsystem.controller;
 
+import com.lambda.pharmacymangementsystem.model.entities.DrugViewEntity;
+import com.lambda.pharmacymangementsystem.model.functions.DrugFunctions;
+import com.lambda.pharmacymangementsystem.utils.Drug;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class AddPurchasesController {
 
@@ -21,30 +27,22 @@ public class AddPurchasesController {
     private TextField customerNameField;
 
     @FXML
-    public void Initialize() {
-        // Initialize the combo box with drug names
+    public void Initialize() throws SQLException {
+        List<DrugViewEntity> drugs = DrugFunctions.getAllDrugs();
+        for (DrugViewEntity drug: drugs ) {
+            drugComboBox.getItems().add(drug.getName());
+        }
 
     }
 
-    private double getPriceForDrug(String drugName) {
 
-        return 10.00;
-    }
 
     @FXML
-    public void updateTotalPrice(ActionEvent actionEvent) {
+    public void updateTotalPrice(ActionEvent event) {
 
     }
 
-    public void addPurchase(ActionEvent actionEvent) {
-
-    }
-
-    private void calculateTotalPrice() {
-
-    }
-
-    private void clearForm() {
+    public void addPurchase(ActionEvent event) {
 
     }
 }
