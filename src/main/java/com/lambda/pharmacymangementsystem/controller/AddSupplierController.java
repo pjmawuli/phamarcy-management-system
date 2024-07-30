bc1818289f5883ece4374788b703034c3ae43067
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-07-30 18:35:09 +0000

    Style add-drugs-view form

--- src/main/java/com/lambda/pharmacymangementsystem/controller/AddSupplierController.java
+++ src/main/java/com/lambda/pharmacymangementsystem/controller/AddSupplierController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
